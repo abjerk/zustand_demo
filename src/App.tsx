8c0c9a689d13824ed6e98898050e4be33620575b
andy <abjerk@kipsu.com>
2024-09-23 10:48:33 -0500

    more theme toggle state info

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React from 'react';
-import {useFormStore, useThemeStore} from './store.ts';  // Adjust the path as necessary
+import { useFormStore, useThemeStore } from './store.ts';  // Adjust the path as necessary
 import './App.css';
 import Form from './components/Form.tsx';
 
@@ -9,17 +9,18 @@ const App: React.FC = () => {
   const { name, email, setName, setEmail } = useFormStore();
 
 
-  if (name && email){
+  if (name && email) {
     console.log(name, ' ', email)
   }
 
   return (
     <div className={`App ${themeClass}`}>
       <div className='theme_section'>
-      <h1>Theme State with Zustand</h1>
-      <button onClick={toggleTheme} className={`${themeClass}`}>
-        {isDarkMode ? 'too dark!' : 'ah! too bright!'}
-      </button>
+        <h1>Theme State with Zustand</h1>
+        <h3 className={`${themeClass}`}>State: isDarkMode = {isDarkMode ? "true" : "false"}</h3>
+        <button onClick={toggleTheme} className={`${themeClass}`}>
+          {isDarkMode ? 'too dark!' : 'ah! too bright!'}
+        </button>
       </div>
       <Form></Form>
     </div>
